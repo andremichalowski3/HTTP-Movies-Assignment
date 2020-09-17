e28f247ef87edc77ab4701876c4f20f8b804ce26
amichalo421 <andre.michalowski@gmail.com>
2020-09-17 17:19:14 -0400

    Task 2B: Create a component with updateMovieForm

---
+++ client/src/Movies/UpdateMovie.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const UpdateMovie = props => {
+  return (
+    <form>
+      <input>
+      </input>
+
+      <input>
+      </input>
+
+      <input>
+      </input>
+    </form>
+  )
+}
+
+export default UpdateMovie;
